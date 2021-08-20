c6defba9dd3ba51cfd755849cec8ed375e93a038
xuhongbin <gmahsm@163.com>
2021-08-20 16:23:50 +0800

    测试
    测试分支本地未提交文件是否可以和主干未提交文件合并,这是主干

--- src/main/java/com/xuhongbin/contoller/Test123.java
+++ src/main/java/com/xuhongbin/contoller/Test123.java
@@ -7,4 +7,7 @@ package com.xuhongbin.contoller;
  * @Date: 2021-08-20 16:16
  **/
 public class Test123 {
+    public static void main(String[] args) {
+        System.out.println("测试分支本地未提交文件是否可以和主干未提交文件合并,这是主干");
+    }
 }
