8be27f997ee3ba190aba2a558ea578ccf06c71e1
xuhongbin <gmahsm@163.com>
2021-08-20 16:22:38 +0800

    测试分支本地未提交文件是否可以和主干未提交文件合并

--- src/main/java/com/xuhongbin/contoller/TestCommit.java
+++ src/main/java/com/xuhongbin/contoller/TestCommit.java
@@ -9,6 +9,6 @@ package com.xuhongbin.contoller;
 public class TestCommit {
 
     public static void main(String[] args) {
-        
+        System.out.println("测试分支本地未提交文件是否可以和主干未提交文件合并");
     }
 }
