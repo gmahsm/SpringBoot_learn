3f1482eb3cd78a58ae0006a8773f811f7d7fc792
xuhongbin <gmahsm@163.com>
2021-08-24 17:38:41 +0800

    ---------

--- src/main/java/com/xuhongbin/contoller/HelloWorld.java
+++ src/main/java/com/xuhongbin/contoller/HelloWorld.java
@@ -2,7 +2,10 @@ package com.xuhongbin.contoller;
 
 import com.xuhongbin.vo.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -11,18 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author: xuhongbin
  * @Date: 2021-08-13 16:18
  **/
-@RestController
+@Controller
 public class HelloWorld {
 
     @Autowired
     Person person;
 
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
     @RequestMapping("/hello")
+    @ResponseBody
     public String helloWorld() {
         return "Hello World";
     }
 
     @RequestMapping("/getPerson")
+    @ResponseBody
     public String getPerson() {
         System.out.println("有人获取Person");
         return person.toString();
